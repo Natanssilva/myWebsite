2acea59377f8c654323dbfea13022a3352c66c05
Natan Porto <natanssilva10@gmail.com>
2023-08-08 19:01:18 -0300

    cursor personalizado

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// Criando um cursor personalizado baseado no movimento da página
+
+const NewCursor = document.getElementById('cursor'); // usando o DOM pra pegar o elemento html 
+
+function attCursor(position) { // criando uma função com uma variavel position que será o parametro, e nessa função vai trazer a posição do cursor
+    NewCursor.style.top = `${position.clientY}px `; // aqui pega a posição da vertical, clientY é um evento de mouseMove
+    NewCursor.style.left = `${position.clientX}px `; //aqui pega a posição horizontal, clientY é um evento de mouseMove
+}
+
+window.addEventListener('mousemove', function (position)  { // aqui eu adiciono um evento de movimento de mouse na janela (NAVEGADOR) e passo o evento pelo parametro
+     attCursor (position); //dentro do método addEventListener eu chamo a função, ou seja sempre que esse evento acontecer vai ser chamado a função para construir a posição do mouse
+});
+
+
