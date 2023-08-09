7992661821252d827337757d6fa00969715ebdec
Natan Porto <natanssilva10@gmail.com>
2023-08-08 22:09:06 -0300

    style menu

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 // Criando um cursor personalizado baseado no movimento da página
 
-const NewCursor = document.getElementById('cursor'); // usando o DOM pra pegar o elemento html 
+const NewCursor = document.getElementById('cursor'); // usando o DOM pra pegar o elemento html e atribuir a uma variavel
 
 function attCursor(position) { // criando uma função com uma variavel position que será o parametro, e nessa função vai trazer a posição do cursor
     NewCursor.style.top = `${position.clientY}px `; // aqui pega a posição da vertical, clientY é um evento de mouseMove
@@ -11,4 +11,6 @@ window.addEventListener('mousemove', function (position)  { // aqui eu adiciono 
      attCursor (position); //dentro do método addEventListener eu chamo a função, ou seja sempre que esse evento acontecer vai ser chamado a função para construir a posição do mouse
 });
 
+
+//Menu 
 
