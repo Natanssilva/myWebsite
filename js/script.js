2008f5e4e11915aadd77ecf6572027a0554c6997
Natan Porto <natanssilva10@gmail.com>
2023-08-10 00:21:02 -0300

    animação de texto

--- js/script.js
+++ js/script.js
@@ -37,4 +37,48 @@ document.getElementById('img-openMenu').addEventListener('click', MenuWeb);
 document.getElementById('img-closeMenu').addEventListener('click', MenuWebClose);
 
 
+//Texto HOME
+//Usando conceito de funções recursivas, são funções  que chamam a si mesma até encontrar uma instrução de parar
+
+const arrayText = [
+    "Sou desenvolvedor Front-End Web",
+    "e faço faculdade de Sistemas para Internet"
+  ];
+  
+  const writeTime = 100; // tempo  escrever e apagar.
+  const changeTextTime = 1000; // tempo para ir para próxima frase no array
+  
+  let indexSentence = 0;
+  let indexChar = 0;
+  
+  const element = document.querySelector("#text-content");
+  
+  function writeText() {
+    if (indexChar <= arrayText[indexSentence].length) {
+      element.textContent = arrayText[indexSentence].substring(0, indexChar);
+      indexChar++;
+      setTimeout(writeText, writeTime);
+    } else {
+      setTimeout(removeText, changeTextTime);
+    }
+  }
+  
+  function removeText() {
+    if (indexChar >= 0) {
+      element.textContent = arrayText[indexSentence].substring(0, indexChar);
+      indexChar--;
+      setTimeout(removeText, writeTime);
+    } else {
+      indexSentence++;
+      if (indexSentence >= arrayText.length) {
+        indexSentence = 0;
+      }
+      setTimeout(writeText, changeTextTime);
+    }
+  }
+  
+  writeText();
+
+
+
 
